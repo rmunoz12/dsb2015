5d7690a588eb6e3b94f84a121a21f69140c8d1cb
Richard Munoz <rmunoz@cs.columbia.edu>
2016-01-23 21:25:34 -0500

    - gen_frame_paths() (previously gen_frames()) now yields a generator - merge gen_norm_frames into gen_augmented_frames() - documentation

--- src/Preprocessing.py
+++ src/Preprocessing.py
@@ -19,15 +19,27 @@ from skimage import transform
 
 
 class Frame(namedtuple('Frame', 'data func aug_name')):
+    """
+    Represents a frame (30 dicom images), along with the data augmentation
+    function that will be applied to the frame.
 
+    Parameters
+    ----------
+    data : list[str]
+        List of paths to the images in the frame
+
+    func :  np.array[float] -> np.array[float]
+        Data augmentation function. The input and output arrays are 2D.
+
+    aug_name : str
+        augmentation function name
+    """
     def __lt__(self, other):
         return self.data < other.data
 
 
-
-def get_frames(root_path):
+def gen_frame_paths(root_path):
     """Get path to all the frame in view SAX and contain complete frames"""
-    ret = []
     for root, _, files in os.walk(root_path):
         if len(files) == 0 or not files[0].endswith(".dcm") or root.find("sax") == -1:
             continue
@@ -35,68 +47,53 @@ def get_frames(root_path):
         fileset = set(files)
         expected = ["%s-%04d.dcm" % (prefix, i + 1) for i in range(30)]
         if all(x in fileset for x in expected):
-            ret.append([root + "/" + x for x in expected])
-    # sort for reproducibility
-    return sorted(ret, key=lambda x: x[0])
+            yield [root + "/" + x for x in expected]
 
 
-def get_label_map(fname):
-    labelmap = {}
-    fi = open(fname)
-    fi.readline()
-    for line in fi:
-        arr = line.split(',')
-        labelmap[int(arr[0])] = line
-    return labelmap
+def get_label_map(file_path):
+    """
+    Build a dictionary for looking up training labels.
 
+    Parameters
+    ----------
+    file_path : str
 
-def write_label_csv(fname, frames, label_map):
-    fo = open(fname, "w")
-    for lst in frames:
-        index = int(lst[0].split("/")[3])
-        if label_map != None:
-            fo.write(label_map[index])
-        else:
-            fo.write("%d,0,0\n" % index)
-    fo.close()
-
-
-def write_data_csv(fname, frames, preproc):
-    """Write data to csv file"""
-    fdata = open(fname, "w")
-    dwriter = csv.writer(fdata)
-    counter = 0
-    result = []
-    for lst in frames:
-        data = []
-        for path in lst:
-            f = dicom.read_file(path)
-            img = preproc(f.pixel_array.astype(float) / np.max(f.pixel_array))
-            dst_path = path.rsplit(".", 1)[0] + ".64x64.jpg"
-            scipy.misc.imsave(dst_path, img)
-            result.append(dst_path)
-            data.append(img)
-        data = np.array(data, dtype=np.uint8)
-        data = data.reshape(data.size)
-        dwriter.writerow(data)
-        counter += 1
-        if counter % 100 == 0:
-            print("%d slices processed" % counter)
-    print("All finished, %d slices in total" % counter)
-    fdata.close()
-    return result
+    Returns
+    -------
+    label_map : dict[str, str]
+        The key is the class number (as an int).
+    """
+    label_map = {}
+    with open(file_path) as fi:
+        fi.readline()  # ignore header
+        for line in fi:
+            arr = line.split(',')
+            label_map[int(arr[0])] = line
+    return label_map
 
 
 def crop_resize(img, size):
-    """crop center and resize"""
-    if img.shape[0] < img.shape[1]:
-        img = img.T
-    # we crop image from center
+    """
+    Crop image `img` into a square with side length `size`. Cropping is
+    performed from the center of the image. Furthermore, the image pixel values
+    are converted from floats to integers from 0 to 255.
+
+    Parameters
+    ----------
+    img : np.array
+        2D array
+
+    size : int
+
+    Returns
+    -------
+    resized_img : np.array[np.dtype('uint8')]
+        2D array
+    """
     short_edge = min(img.shape[:2])
     yy = int((img.shape[0] - short_edge) / 2)
     xx = int((img.shape[1] - short_edge) / 2)
     crop_img = img[yy: yy + short_edge, xx: xx + short_edge]
-
     resized_img = transform.resize(crop_img, (size, size))
     resized_img *= 255
     return resized_img.astype("uint8")
@@ -167,42 +164,55 @@ def split_csv(src_csv, split_to_train, train_csv, test_csv):
     ftest.close()
 
 
-def get_aug_funcs():
-    deg = random.random() * 360
-    return [(lambda x: crop_resize(x, 128), 'n'),
-            (lambda x: crop_resize(rotate(x, deg), 128), 'r'),
-            (lambda x: crop_resize(flip(x), 128), 'f'),
-            (lambda x: crop_resize(flip(rotate(x, deg)), 128), 'rf')]
+def get_aug_funcs(normal_only=False):
+    funcs = [(lambda x: crop_resize(x, 128), 'n')]
+    if not normal_only:
+        deg = random.random() * 360
+        funcs.extend([(lambda x: crop_resize(rotate(x, deg), 128), 'r'),
+                      (lambda x: crop_resize(flip(x), 128), 'f'),
+                      (lambda x: crop_resize(flip(rotate(x, deg)), 128), 'rf')])
+    return funcs
 
 
-def gen_augmented_frames(frames):
-    for lst in frames:
-        funcs = get_aug_funcs()
+def gen_augmented_frames(paths, normal_only=False):
+    """
+    Generates a `Frame` that includes the paths to its images as well as the
+    data augmentation function that should applied to the frame.
+
+    Parameters
+    ----------
+    paths : list[str]
+        Paths tot he dicom images of the frame
+
+    normal_only : bool
+        If true, will only return the 'normal' augmentation function, which
+        only crops the input image to the size needed for the training network.
+
+    Yields
+    ------
+    Frame
+    """
+    for lst in paths:
+        funcs = get_aug_funcs(normal_only)
         for f, name in funcs:
             yield Frame(data=lst, func=f, aug_name=name)
 
 
-def gen_norm_frames(paths):
-    for lst in paths:
-        yield Frame(data=lst, func=lambda x: crop_resize(x, 128), aug_name='n')
-
-
 random.seed(100)
-train_paths = get_frames("../data/train")
-vld_paths = get_frames("../data/validate")
+train_paths = gen_frame_paths("../data/train")
+vld_paths = gen_frame_paths("../data/validate")
 
 os.makedirs("../output/", exist_ok=True)
 
 train_frames = gen_augmented_frames(train_paths)
 train_frames = sorted(train_frames)  # for reproducibility
-# random.shuffle(train_frames)
+random.shuffle(train_frames)
 write_data_and_label_csvs('../output/train-64x64-data.csv', '../output/train-label.csv', train_frames, get_label_map('../data/train.csv'))
 
 
-vld_frames = gen_norm_frames(vld_paths)
-# write_label_csv("../output/validate-label.csv", vld_paths, None)
-# write_data_csv("../output/validate-64x64-data.csv", vld_paths, lambda x: crop_resize(x, 128))
+vld_frames = gen_augmented_frames(vld_paths, normal_only=True)
 write_data_and_label_csvs("../output/validate-64x64-data.csv", "../output/validate-label.csv", vld_frames, None)
+
 
 # Generate local train/test split, which you could use to tune your model locally.
 train_index = np.loadtxt("../output/train-label.csv", delimiter=",")[:,0].astype("int")
