720575672af7857aef6a44b67374b529cb61a442
Richard Munoz <rmunoz@cs.columbia.edu>
2016-01-26 01:29:09 -0500

    path fixes

--- dsb2015/config.py
+++ dsb2015/config.py
@@ -5,26 +5,28 @@ class Config(object):
     """
 
     out_files = \
-        {'train_data_csv': '../output/train-64x64-data.csv',
-         'train_label_csv': '../output/train-label.csv',
-         'train_systole_out': '../output/train-stytole.csv',
-         'train_diastole_out': '../output/train-diastole.csv',
-         'valid_data_csv': '../output/validate-64x64-data.csv',
-         'valid_label_out': '../output/validate-label.csv',
-         'sample_submit': '../data/sample_submission_validate.csv',
-         'submit_out': '../output/submission.csv',
+        {'train_data_csv': 'train-64x64-data.csv',
+         'train_label_csv': 'train-label.csv',
+         'train_systole_out': 'train-stytole.csv',
+         'train_diastole_out': 'train-diastole.csv',
+         'valid_data_csv': 'validate-64x64-data.csv',
+         'valid_label_out': 'validate-label.csv',
+         'sample_submit': 'sample_submission_validate.csv',
+         'submit_out': 'submission.csv',
          'model_out': '',       # TODO save model output
          'test_data_path': ''}  # TODO load test_data path (round 2)
 
     def __init__(self, **kwargs):
         for key in kwargs:
             setattr(self, key, kwargs[key])
-        for name in Config.out_files:
-            if self.local and name in {'train_data_csv', 'train_label_csv',
+        for k, v in Config.out_files.items():
+            if self.local and k in {'train_data_csv', 'train_label_csv',
                                        'valid_data_csv', 'valid_label_out'}:
-                setattr(self, name, self.output_path + 'local_' + name)
+                setattr(self, k, self.output_path + 'local_' + v)
+            elif k in {'sample_submit'}:
+                setattr(self, k, self.train_path + v)
             else:
-                setattr(self, name, self.output_path + name)
+                setattr(self, k, self.output_path + v)
 
     def __repr__(self):
         return str(self.__dict__)
--- dsb2015/preprocess.py
+++ dsb2015/preprocess.py
@@ -138,7 +138,7 @@ def split_csv(src_csv, split_to_train, train_csv, test_csv):
     ftest.close()
 
 
-def make_local_split(test_frac, output_path):
+def make_local_split(test_frac, cfg):
     """
     Generate local train/test split, which can be used evaluate models locally,
     blind to the result of submission to Kaggle's validation set.
@@ -153,21 +153,23 @@ def make_local_split(test_frac, output_path):
         Path to output folder, ending with a slash `/`.
     """
     train_index = \
-        np.loadtxt(output_path + "train-label.csv", delimiter=",")[:, 0].\
+        np.loadtxt(cfg.train_label_csv, delimiter=",")[:, 0].\
             astype("int")
     train_index = set(train_index)
     num_test = int(len(train_index) * test_frac)
     random.shuffle(list(train_index))
     train_index = set(train_index[num_test:])
     split_to_train = [x in train_index for x in train_index]
-    split_csv(output_path + "train-label.csv",
+    orig_path = cfg.train_label_csv.rsplit('/', 1)
+    split_csv(cfg.train_label_csv,
               split_to_train,
-              output_path + "local_train-label.csv",
-              output_path + "local_test-label.csv")
-    split_csv(output_path + "train-64x64-data.csv",
+              orig_path[0] + 'local_' + orig_path[1],
+              orig_path[0] + 'local_test' + orig_path[1][5:])  # remove 'train'
+    orig_path = cfg.train_data_csv.rsplit('/', 1)
+    split_csv(cfg.train_data_csv,
               split_to_train,
-              output_path + "local_train-64x64-data.csv",
-              output_path + "local_test-64x64-data.csv")
+              orig_path[0] + 'local_' + orig_path[1],
+              cfg.output_path + 'local_test' + orig_path[1][5:])  # remove 'train'
 
 
 def preprocess(cfg):
@@ -185,13 +187,13 @@ def preprocess(cfg):
     train_frames = gen_augmented_frames(train_paths, 128)
     train_frames = sorted(train_frames)  # for reproducibility
     random.shuffle(train_frames)
-    write_data_and_label_csvs(cfg.output_path + 'train-64x64-data.csv',
-                              cfg.output_path + 'train-label.csv',
+    write_data_and_label_csvs(cfg.train_data_csv,
+                              cfg.train_label_csv,
                               train_frames,
                               get_label_map(cfg.train_path + 'train.csv'))
     vld_frames = gen_augmented_frames(vld_paths, 128, normal_only=True)
-    write_data_and_label_csvs(cfg.output_path + "validate-64x64-data.csv",
-                              cfg.output_path + "validate-label.csv",
+    write_data_and_label_csvs(cfg.valid_data_csv,
+                              cfg.valid_label_out,
                               vld_frames,
                               None)
-    make_local_split(0.1, cfg.output_path)
+    make_local_split(0.1, cfg)
